047b8f118794281f9d15936aaff02c5c0e182055
Miranda Ramsey <miranda.j.ramsey@gmail.com>
2025-12-30 13:53:37 -0800

    Latest build deployed.

---
+++ lunr-pretext-search-index.js
@@ -0,0 +1,122 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "frontmatter",
+  "level": "1",
+  "url": "frontmatter.html",
+  "type": "Front Matter",
+  "number": "",
+  "title": "Front Matter",
+  "body": "  Miranda Ramsey Portland Community College      My Website   copyright         "
+},
+{
+  "id": "sec-course-information",
+  "level": "1",
+  "url": "sec-course-information.html",
+  "type": "Section",
+  "number": "1",
+  "title": "Course Information",
+  "body": " Course Information      Title  Math Literacy I    Number  MTH 58    CRN  11475    Credits  4    Term  Winter 2026    Prerequisites  MTH 20 and (WR 80 or ESOL 252) and (RD 80 or ESOL 250) or equivalent placement.    Times  Mondays and Wednesdays, 2:00PM - 4:20PM    Location  In Person, SCOM 314, Southeast Campus       Course Description   Explores how to clearly communicate arguments supported by quantitative evidence. Investigates contextual and open-ended mathematical problems. Develops mathematical reasoning skills through interpreting information, making conjectures, communicating effectively, and verifying results. Emphasizes an understanding of the role of mathematics and how it affects decision making in life. Uses collaborative learning through in-class group interaction. Uses technology throughout the course. Audit available. Read the Course Content and Outcomes Guide here .    Learning Outcomes  Upon successful completion of this course, students should be able to:  Use and integrate several different types of technology to explore and analyze data to solve problems.  Use contextual information to reflect on and communicate the reasonableness of mathematical approaches and solutions.  Use quantitative reasoning, articulate individual positions, and respectfully consider the approaches and solutions of others to contribute to the academic conversation.  Critically analyze information for its accuracy, relevance, and credibility to reflect on how mathematics can be used in one’s life and in the larger community.  Engage with and formulate questions to explore mathematical topics, collaborate with others, and persevere through the problem solving process.  Develop self-awareness of the learning process and self-monitor understanding and performance.      "
+},
+{
+  "id": "sec-instructor",
+  "level": "1",
+  "url": "sec-instructor.html",
+  "type": "Section",
+  "number": "2",
+  "title": "Instructor Information",
+  "body": " Instructor Information      Instructor  Miranda Jane Ramsey    Email   miranda.ramsey@pcc.edu     Phone  971-722-3542    Office Location  SCOM 214    School Address  Portland Community College, 12000 SW 49th Ave., Portland, OR 97219        Communication   Best Way to Contact Me  I can best be reached by email. I try to reply the same day when I can, and I will generally reply before the end of the next school day.     Schedule  This term I am teaching the following classes:   Term Schedule           Class  CRN  Days  Time  Place  Midterm  Final    STAT 243Z  13446  MW  9:00 11:50  TABOR 142  February 9  March 16    MTH 58  11475  MW  2:00 4:20  SCOM 314   February 4  February 25   March 18    MTH 111Z  11221  TuTh  9:00 11:50  TABOR 143  February 5  March 12    MTH 231  -  TuTh  1:00 3:40  Zoom        Office Hours       Day  Time  Place    Mondays  12-1  SCOM 214    Wednesdays  12-1  SCOM 214    Fridays  By Appointment  Zoom      "
+},
+{
+  "id": "sec-instructor-4-3",
+  "level": "2",
+  "url": "sec-instructor.html#sec-instructor-4-3",
+  "type": "Table",
+  "number": "2.1",
+  "title": "Term Schedule",
+  "body": " Term Schedule           Class  CRN  Days  Time  Place  Midterm  Final    STAT 243Z  13446  MW  9:00 11:50  TABOR 142  February 9  March 16    MTH 58  11475  MW  2:00 4:20  SCOM 314   February 4  February 25   March 18    MTH 111Z  11221  TuTh  9:00 11:50  TABOR 143  February 5  March 12    MTH 231  -  TuTh  1:00 3:40  Zoom      "
+},
+{
+  "id": "sec-instructor-4-4",
+  "level": "2",
+  "url": "sec-instructor.html#sec-instructor-4-4",
+  "type": "Table",
+  "number": "2.2",
+  "title": "Office Hours",
+  "body": " Office Hours       Day  Time  Place    Mondays  12-1  SCOM 214    Wednesdays  12-1  SCOM 214    Fridays  By Appointment  Zoom    "
+},
+{
+  "id": "sec-materials",
+  "level": "1",
+  "url": "sec-materials.html",
+  "type": "Section",
+  "number": "3",
+  "title": "Materials",
+  "body": " Materials   Textbooks  We will be using the textbook Pathways to Math Literacy, 3rd edition. This book comes with an ALEKS code that you will need to complete homework in the class. It is very important that you begin the class with a fresh copy of the textbook and an ALEKS access code. If this is your second time taking the class, let me know so we can figure out what you can use again, and what you need to purchase.    Software\/Hardware\/Equipment  We will be using a number of software applications in this class. To access these outside of our classroom, you will need to have access to a personal computer with a recommended browser installed. We will be accessing the following:    D2L Brightspace , our primary learning platform with PCC,     ALEKS , where you will complete weekly homework assignments,     Google Drive and associated apps, especially Google Sheets.     Please visit the technology support section of Student Support Resources for a complete list of apps and tools for PCC students.    Accessibility  This course uses digital courseware, software, or texts that may cause barriers for those using assistive, or accessibility-related technologies. If you encounter barriers, please contact the Access Tech Team at access-tech-group@pcc.edu or by calling 971-722-8324.   "
+},
+{
+  "id": "sec-attendance",
+  "level": "1",
+  "url": "sec-attendance.html",
+  "type": "Section",
+  "number": "4",
+  "title": "Attendance and Participation",
+  "body": " Attendance and Participation   Students are expected to attend each scheduled class period. Credit will be given each day for:   Arriving on time with all necessary materials. (2 points)    Engaging with the group during the lab portion. (2 points)    Staying for the full duration of the class. (1 point)       First Week Activities  During the first week of the term, instructors must record students who are not in attendance or engaging in the class. Students who have not done the following will be dropped according to PCC's No Show Policy :   Attend all of the first days of class up to the drop deadline.    Enroll in the ALEKS course and complete the first week's assignments.    Initialize your Google Sheets spreadsheet assignment using the template in D2L.   If you are unable to complete one of these requirements but want to remain enrolled in the class, you must speak to me to make an alternative arrangement to show engagement in the course.   "
+},
+{
+  "id": "sec-grading",
+  "level": "1",
+  "url": "sec-grading.html",
+  "type": "Section",
+  "number": "5",
+  "title": "Grading",
+  "body": " Grading   For more information, please go to the PCC Grading Guidelines .    Graded Assignments     Class Participation  Each day of class you will receive a grade based on arriving on time, having all of your required course materials and notes completed, engaging and participating during the lab time, and staying until the end of class.    ALEKS Homework  Most sections we cover in class will have an associated homework assignment in ALEKS. Additionally, there will be a knowledge check and a Module 1 check assigned.    Discussions  Each non-exam week will have a discussion prompt to respond to in D2L. Discussion prompts will be in two parts:   Create a new thread with your response to the prompt. Each prompt will have instructions for that week's post.    One or more replies to other students' posts. Each prompt will have instructions for that week's replies.       Book Applications  We will be completing worksheets in the textbook as a class, in groups, and on your own. There will be some time in class for this, but you will be expected to spend time out of class completing them if needed. Before each exam, I will collect your textbooks to grade your completeness.    Spreadsheet Assignments  As we progress through the class you will be assigned activities to complete with Google Sheets. There will be some time in class for this, but you will be expected to spend time out of class completing them if needed. I will grade your spreadsheet for completeness twice during the term.    Quizzes  Each non-exam week will have a quiz on the material we covered in class.    Exams  There will be three exams, each covering a different module from the textbook. The exams will be on February 4, February 25, and March 18.    Final Project  Starting in week 7, you will work with a group on a final project, planning out the purchase of a car. Your results will be presented on the last scheduled class day before the final exam.       Late Work and Make-Up Policy  Students are expected to complete all assignments on pace with the course. However, I understand that external things get in the way. I will consider giving up to 1 week extension on assignments when requested before the original due date of the assignment. All assignments must be completed before the final exam, regardless of extension requests.  If you must miss an exam, you must arrange a make up exam with me before the day of the exam.    Criteria  Please read over all assignments and discussion prompts in advance and reach out if you have any questions or if you want to confirm you understand them correctly. Clarifying questions will help you avoid deductions in your grades.       Activities  Number  Weight    Class Participation  16  10%    ALEKS Homework  13  10%    Discussions  8  5%    Book Applications  12  10%    Spreadsheet Assignments  12  5%    Quizzes  8  5%    Midterm 1  1  15%    Midterm 2  1  15%    Final Exam  1  20%    Final Project  1  5%    Total    500         Letter Grade  Points  Percentage    A  450-500  90-100%    B  400-449  80-89%    C  350-399  70-79%    D  300-349  60-69%    F  < 300  < 59%      "
+},
+{
+  "id": "sec-policies",
+  "level": "1",
+  "url": "sec-policies.html",
+  "type": "Section",
+  "number": "6",
+  "title": "PCC Policies and Deadlines",
+  "body": " PCC Policies and Deadlines   Visit the PCC Policies page for information on:   Academic Integrity Policy    Accessibility Statement and Resources    Drop\/Withdraw Deadlines    Grading Policy    Internet Etiquette    PCC Payment Policy    PCC Registration Policy    Sanctuary College    Student Rights and Responsibilities    Title IX\/Non-Discrimination       Drop\/Withdraw Deadlines  A student is responsible to add, drop, or withdraw classes. Please review PCC Registration Policy for more information.   Students need to register online via MyPCC. Please review the Online Registration Instructions to find out how.    The last day to drop this class is Tuesday, Jan 13 . This is also the last day to register with instructor approval.    The last day to withraw is Saturday, March 14 .       Accomodations and Services  PCC is committed to supporting all students. If you plan to use academic accommodations for this course, please contact your instructor as soon as possible to discuss your needs. Accommodations are not retroactive; they begin when the instructor receives the “Approved Academic Accommodations” letter from you (submitted in person for courses on campus; via email for Online Learning courses). To request academic accommodations for a disability, please contact an Accessible Education and Disability Resources counselor on any PCC campus. Office locations, phone numbers, and additional information may be located on the Accessible Education and Disability Resources website .    Student Rights and Responsibilities  Students are required to complete this course in accordance with the Student Rights and Responsibilities Handbook . The Handbook establishes students' freedoms and protections as well as expectations of appropriate behavior and ethical academic work. The Handbook includes items such as the Policy on Student Rights, and the Student Code of Conduct Policy and Procedures.    Academic Integrity   Rules about cheating, plagiarism, or sharing work  The handbook contains the Code of Student Conduct and the Academic Integrity Policy. Cheating includes any attempt to defraud, deceive, or mislead the instructor in arriving at an honest grade assessment, and may include copying answers from other students or using unauthorized notes during tests. Plagiarism is a particular form of cheating that involves presenting as one's own the ideas or work of another and may include using other people's ideas without proper attribution and submitting another person's work as one's own. Dishonest activities such as cheating on exams and submitting or copying work done by others will result in disciplinary actions including but not limited to receiving a failing grade. For further information, review the institution's Academic Integrity Policy .    Generative AI  The use of generative AI tools, including but not limited to ChatGPT, to complete your work is strictly prohibited and will be considered equivalent to unauthorized sharing of work or plagiarism. All AI use is strongly discouraged, even as a study tool. I want to help you build a strong internal understanding of the material without relying on outside tools. If you have a question or need help, please reach out to me instead.  In return, I promise that I will not use generative AI in my relationship with you as your instructor. Any feedback you receive on your work will be my authentic human response to you.     Internet Etiquette  Some of this class may include online discussions or other interactions with students online. Click here for more information about Internet Etiquette (or Netiquette) .    Sanctuary College Statement  PCC is a sanctuary college. For more information and resources, see the resources for undocumented students page .    Inclement Weather Statement  Our course is typically unaffected by college campus and facility closures or delayed class start times. Our regular due dates apply. However, if PCC must close all operations, our regular due dates will be adjusted accordingly. Please check your email for my instructions and continue to check D2L Brightspace during closures.    Flexibility Statement  The instructor reserves the right to modify course content and\/or substitute assignments and learning activities in response to institutional, weather, or class situations.   "
+},
+{
+  "id": "sec-calendar",
+  "level": "1",
+  "url": "sec-calendar.html",
+  "type": "Section",
+  "number": "7",
+  "title": "Calendar",
+  "body": " Calendar  This schedule is a tentative outline and may change to meet the needs of the class.   Course Calendar        Week  Monday  Wednesday  Assignments    1   January 5  Introduction    January 7  Section 1-1  Quiz 1 (Practice)    ALEKS Knowledge Check  W1 Discussion Post (due 1\/7)  W1 Discussion Reply (due 1\/9)  ALEKS 1-1 (Due 1\/11)     2   January 12  Section 1-2    January 14  Section 1-3  Quiz 2    W2 Discussion Post (due 1\/14)  W2 Discussion Reply (due 1\/16)  ALEKS 1-2 (Due 1\/18)  ALEKS 1-3 (Due 1\/18)     3   January 19  No Class  Dr. MLK Jr. Day    January 21  Section 1-4  Quiz 3    W3 Discussion Post (due 1\/21)  W3 Discussion Reply (due 1\/23)  ALEKS Module 1 (Due 1\/23)  ALEKS 1-4 (Due 1\/25)     4   January 26  Section 1-7    January 28  Section 2-2  Quiz 4    W4 Discussion Post (due 1\/28)  W4 Discussion Reply (due 1\/30)  ALEKS 1-7 (Due 2\/1)  ALEKS 2-2 (Due 2\/1)     5   February 2  Exam Review  Book Check 1    February 4  Exam 1       6   February 9  Section 2.3    February 11  Section 2.4  Quiz 5    W6 Discussion Post (due 2\/11)  W6 Discussion Reply (due 2\/13)  ALEKS 2-3 (Due 2\/15)  ALEKS 2-4 pt 1 (Due 2\/15)  Sheets Check 1     7   February 16  Section 2.5  Group Project Intro    February 18  Section 3.1  Quiz 6    W7 Discussion Post (due 2\/18)  W7 Discussion Reply (due 2\/20)  ALEKS 2-4 pt 2 (Due 2\/22)  ALEKS 2-5 (Due 2\/25)     8   February 23  Exam Review  Book Check 2  Group Project Part 1    February 25  Exam 2       9   March 2  Section 3.2  Group Project Part 2    March 4  Section 3.3  Quiz 7    W9 Discussion Post (due 3\/4)  W9 Discussion Reply (due 3\/6)  ALEKS 3-1 (Due 3\/8)  ALEKS 3-2 (Due 3\/8)     10   March 9  Group Project Part 3  Quiz 8  Book Check 3    March 11  Presentations    W10 Discussion Post (due 3\/11)  W10 Discussion Reply (due 3\/13)  Sheets Check 2     Finals   March 16  No Class    March 18  Final Exam   -     "
+},
+{
+  "id": "sec-calendar-3",
+  "level": "2",
+  "url": "sec-calendar.html#sec-calendar-3",
+  "type": "Table",
+  "number": "7.1",
+  "title": "Course Calendar",
+  "body": " Course Calendar        Week  Monday  Wednesday  Assignments    1   January 5  Introduction    January 7  Section 1-1  Quiz 1 (Practice)    ALEKS Knowledge Check  W1 Discussion Post (due 1\/7)  W1 Discussion Reply (due 1\/9)  ALEKS 1-1 (Due 1\/11)     2   January 12  Section 1-2    January 14  Section 1-3  Quiz 2    W2 Discussion Post (due 1\/14)  W2 Discussion Reply (due 1\/16)  ALEKS 1-2 (Due 1\/18)  ALEKS 1-3 (Due 1\/18)     3   January 19  No Class  Dr. MLK Jr. Day    January 21  Section 1-4  Quiz 3    W3 Discussion Post (due 1\/21)  W3 Discussion Reply (due 1\/23)  ALEKS Module 1 (Due 1\/23)  ALEKS 1-4 (Due 1\/25)     4   January 26  Section 1-7    January 28  Section 2-2  Quiz 4    W4 Discussion Post (due 1\/28)  W4 Discussion Reply (due 1\/30)  ALEKS 1-7 (Due 2\/1)  ALEKS 2-2 (Due 2\/1)     5   February 2  Exam Review  Book Check 1    February 4  Exam 1       6   February 9  Section 2.3    February 11  Section 2.4  Quiz 5    W6 Discussion Post (due 2\/11)  W6 Discussion Reply (due 2\/13)  ALEKS 2-3 (Due 2\/15)  ALEKS 2-4 pt 1 (Due 2\/15)  Sheets Check 1     7   February 16  Section 2.5  Group Project Intro    February 18  Section 3.1  Quiz 6    W7 Discussion Post (due 2\/18)  W7 Discussion Reply (due 2\/20)  ALEKS 2-4 pt 2 (Due 2\/22)  ALEKS 2-5 (Due 2\/25)     8   February 23  Exam Review  Book Check 2  Group Project Part 1    February 25  Exam 2       9   March 2  Section 3.2  Group Project Part 2    March 4  Section 3.3  Quiz 7    W9 Discussion Post (due 3\/4)  W9 Discussion Reply (due 3\/6)  ALEKS 3-1 (Due 3\/8)  ALEKS 3-2 (Due 3\/8)     10   March 9  Group Project Part 3  Quiz 8  Book Check 3    March 11  Presentations    W10 Discussion Post (due 3\/11)  W10 Discussion Reply (due 3\/13)  Sheets Check 2     Finals   March 16  No Class    March 18  Final Exam   -    "
+},
+{
+  "id": "backmatter-2",
+  "level": "1",
+  "url": "backmatter-2.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": " This book was authored in PreTeXt .  "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
